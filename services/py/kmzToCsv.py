ab4cd828a74aa3d7e46b5e2ab256f161e2850c91
Federico Schvemler <federico.schvemler@tsgforce.com>
2020-02-03 17:25:05 +0000

    changes on kmz to csv script, adding name column to csv

--- services/py/kmzToCsv.py
+++ services/py/kmzToCsv.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import csv
 
 def split_coordinates(str):
-    print(str)
+    
     space_splits = str.strip().split(",")
     return [space_splits[1],space_splits[0],space_splits[2]]
 
@@ -10,9 +10,14 @@ def main():
     with open('test.kml', 'r') as f:
         s = BeautifulSoup(f, 'xml')
         with open('out.csv', 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            for coords in s.find_all('coordinates'):
-                writer.writerow(split_coordinates(coords.string))
+            writer = csv.writer(csvfile, dialect='unix')
+            for coords in s.find_all('Placemark'):
+                row = split_coordinates(coords.find('coordinates').string)
+                row.append(coords.find('name').string)
+                writer.writerow(row)
+                
+                
 
 if __name__ == "__main__":
     main()
+    
