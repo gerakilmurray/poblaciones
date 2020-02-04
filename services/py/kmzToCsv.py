06c17669d47036174d564cf878c75d110dbff1ff
Fernando <fernando.e.nitz@tsgforce.com>
2020-02-04 12:16:16 +0000

    adding kmz conversion- finding a bug in test3.kmz

--- services/py/kmzToCsv.py
+++ services/py/kmzToCsv.py
@@ -1,24 +1,44 @@
 from bs4 import BeautifulSoup
+from zipfile import ZipFile
 import csv
+import shutil
+import os
 
 def split_coordinates(str):
-    
     space_splits = str.strip().split(",")
-    return [space_splits[1],space_splits[0],space_splits[2]]
+    return [space_splits[0],space_splits[1],space_splits[2]]
+
 
 def main():
-    with open('test.kml', 'r') as f:
-        s = BeautifulSoup(f, 'xml')
-        with open('out.csv', 'w') as csvfile:
-            writer = csv.writer(csvfile, dialect='unix')
-            writer.writerow(["Coordinates","Name"])
-            for coords in s.find_all('Placemark'):
-                row = split_coordinates(coords.find('coordinates').string)
-                row.append(coords.find('name').string)
-                writer.writerow(row)
-                
-                
+    process_kmz('test.kmz')
+    #process_kml('test.kml')
 
+def process_kmz(file_name):
+    kmz=  ZipFile(file_name,'r')
+    tmp_dir = os.getcwd() + '/tmp'
+    kmz.extractall(tmp_dir)
+    for subdir, dirs, files in os.walk(tmp_dir):
+        for file in files:
+            name, extension = os.path.splitext(file)
+            if extension == '.kml':
+                process_kml(tmp_dir + '/' + file)
+    #shutil.rmtree(tmp_dir)
+    
+def process_kml(file_name):
+    with open(file_name, 'r') as f:
+        s = BeautifulSoup(f, 'xml')
+        with open(file_name + 'out.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, dialect='unix')
+            writer.writerow(["Latitude","Longitude","Altitude","Name"])
+            for coords in s.find_all('Placemark'):
+                coordinates = coords.find('coordinates')
+                name = coords.find('name')
+                if coordinates is not None:
+                    row = split_coordinates(coordinates.string)
+                    if name is not None:
+                        row.append(name.string)
+                    writer.writerow(row)
+    
 if __name__ == "__main__":
     main()
     
