aa1389a95f1c8c87d3009112aefc410009352d6d
Gerardo Kilmurray <gerardo.l.kilmurray@tsgforce.com>
2020-03-09 20:01:49 -0300

    Changes in installation scripts

--- build/build_azure.sh
+++ build/build_azure.sh
@@ -2,29 +2,34 @@
 # fileencoding=utf8
 # lineends=linux
 
-echo -e "\n### Installing dependencies ################################################################################################"
+echo -e "\n### Installing NPM dependencies #############################################################################################"
 cd ~/ffg_dev/rcr_repo/frontend
 npm install
+
+echo -e "\n### Installing PHP dependencies #############################################################################################"
 cd ~/ffg_dev/rcr_repo/services
 php composer.phar install
 
+echo -e "\n### Copying configuration files #############################################################################################"
 cp -v ~/ffg_dev/rcr_repo/build/configs_azure/settings.php ~/ffg_dev/rcr_repo/services/config
 cp -v ~/ffg_dev/rcr_repo/build/configs_azure/.htaccess ~/ffg_dev/rcr_repo/services/web
 cp -v ~/ffg_dev/rcr_repo/build/configs_azure/dev.env.js ~/ffg_dev/rcr_repo/frontend/config
 cp -v ~/ffg_dev/rcr_repo/build/configs_azure/index.js ~/ffg_dev/rcr_repo/frontend/config
 
-echo -e "\n### Compiling local release ################################################################################################"
-
+echo -e "\n### Cleaning local release #################################################################################################"
 cd ~/ffg_dev/rcr_repo/build
 sudo rm -Rf ./release
 
 mkdir -p ../services/storage/temp/
 touch ../services/storage/temp/XDEBUG_SESSION.txt.lock
 
+echo -e "\n### Compiling local release ################################################################################################"
 ./build.sh vendor
 
+echo -e "\n### Removing temp files ####################################################################################################"
 rm -v release.tar.bz2
 
+echo -e "\n### Copying configuration files ############################################################################################"
 cp -v ./configs_azure/settings.php ./release/config
 cp -v ./configs_azure/.htaccess ./release/web
 mkdir -vp ./release/storage/temp
